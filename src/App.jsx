63d0e4ea8c31619808702a606929bc79187ab1f0
komarnetwork <65493176+komarnetwork@users.noreply.github.com>
2024-04-03 13:29:35 +0700

    setup default style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-function App() {
+// components
+import Banner from './components/Banner'
+import Header from './components/Header'
+import Nav from './components/Nav'
+import About from './components/About'
+import Services from './components/Services'
+import Work from './components/Work'
+import Contact from './components/Contact'
+
+const App = () => {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className='overflow-hidden bg-no-repeat bg-cover bg-site'>
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
+      <div className='h-[4000px]'></div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: 'Orbitron',
+      secondary: 'Nunito',
+      tertiary: 'Aldrich',
+    },
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    extend: {
+      colors: {
+        primary: '#0a0a0a',
+        accent: '#B809C3',
+      },
+      backgroundImage: {
+        site: "url('./assets/site-bg.jpg')",
+        about: "url('./assets/about.png')",
+        services: "url('./assets/services.png')",
+      },
+    },
   },
   plugins: [],
 }
