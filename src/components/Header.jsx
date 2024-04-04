8965f4e6a8a4dde91c35ee10621e03ca6bb4dc24
komarnetwork <65493176+komarnetwork@users.noreply.github.com>
2024-04-04 09:03:35 +0700

    rm element jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,16 @@ import Logo from '../assets/logo.svg'
 
 const Header = () => {
   return (
-    <>
-      <header className='px-4 py-8'>
-        <div className='container mx-auto'>
-          <div className='flex items-center justify-between'>
-            <a href='#'>
-              <img src={Logo} alt='MyLogo' />
-            </a>
-            <button className='btn btn-sm'>Work with me</button>
-          </div>
+    <header className='px-4 py-8'>
+      <div className='container mx-auto'>
+        <div className='flex items-center justify-between'>
+          <a href='#'>
+            <img src={Logo} alt='MyLogo' />
+          </a>
+          <button className='btn btn-sm'>Work with me</button>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,65 +4,63 @@ import { Link } from 'react-scroll'
 
 const Nav = () => {
   return (
-    <>
-      <div className='fixed z-50 w-full overflow-hidden bottom-2 lg:bottom-8'>
-        <div className='container mx-auto'>
-          <div className='flex items-center justify-between w-full h-12 px-5 mx-auto rounded-full bg-black/20 backdrop-blur-2xl max-w-80 text-white/50'>
-            <Link
-              to='home'
-              activeClass='active'
-              smooth={true}
-              spy={true}
-              duration={500}
-              offset={-200}
-              className='flex items-center justify-center w-12 h-12 cursor-pointer'
-            >
-              <BiHomeAlt />
-            </Link>
-            <Link
-              to='about'
-              activeClass='active'
-              smooth={true}
-              spy={true}
-              duration={500}
-              className='flex items-center justify-center w-12 h-12 cursor-pointer'
-            >
-              <BiUser />
-            </Link>
-            <Link
-              to='services'
-              activeClass='active'
-              smooth={true}
-              spy={true}
-              duration={500}
-              className='flex items-center justify-center w-12 h-12 cursor-pointer'
-            >
-              <BsClipboardData />
-            </Link>
-            <Link
-              to='work'
-              activeClass='active'
-              smooth={true}
-              spy={true}
-              duration={500}
-              className='flex items-center justify-center w-12 h-12 cursor-pointer'
-            >
-              <BsBriefcase />
-            </Link>
-            <Link
-              to='contact'
-              activeClass='active'
-              smooth={true}
-              spy={true}
-              duration={500}
-              className='flex items-center justify-center w-12 h-12 cursor-pointer'
-            >
-              <BsChatSquareText />
-            </Link>
-          </div>
+    <div className='fixed z-50 w-full overflow-hidden bottom-2 lg:bottom-8'>
+      <div className='container mx-auto'>
+        <div className='flex items-center justify-between w-full h-12 px-5 mx-auto rounded-full bg-black/20 backdrop-blur-2xl max-w-80 text-white/50'>
+          <Link
+            to='home'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            duration={500}
+            offset={-200}
+            className='flex items-center justify-center w-12 h-12 cursor-pointer'
+          >
+            <BiHomeAlt />
+          </Link>
+          <Link
+            to='about'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            duration={500}
+            className='flex items-center justify-center w-12 h-12 cursor-pointer'
+          >
+            <BiUser />
+          </Link>
+          <Link
+            to='services'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            duration={500}
+            className='flex items-center justify-center w-12 h-12 cursor-pointer'
+          >
+            <BsClipboardData />
+          </Link>
+          <Link
+            to='work'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            duration={500}
+            className='flex items-center justify-center w-12 h-12 cursor-pointer'
+          >
+            <BsBriefcase />
+          </Link>
+          <Link
+            to='contact'
+            activeClass='active'
+            smooth={true}
+            spy={true}
+            duration={500}
+            className='flex items-center justify-center w-12 h-12 cursor-pointer'
+          >
+            <BsChatSquareText />
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
